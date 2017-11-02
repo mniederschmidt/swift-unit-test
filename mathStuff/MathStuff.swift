55366f9a29f89b16794e3c72109eb89c5a1bcbb9
Ryan Pliske <ryan.pliske@monsanto.com>
2017-11-01 19:10:43 -0500

    first commit

---
+++ mathStuff/MathStuff.swift
@@ -0,0 +1,9 @@
+//
+//  MathStuff.swift
+//  mathStuff
+//
+//  Created by PLISKE, RYAN [AG-Contractor/1000] on 11/1/17.
+//  Copyright © 2017 tplisk. All rights reserved.
+//
+
+import Foundation
---
+++ mathStuffTests/HumanizeTests.swift
@@ -0,0 +1,27 @@
+import XCTest
+@testable import iOSTechExercises
+
+class HumanizeTests: XCTestCase {
+    let humanize = Humanize()
+    
+    func testReturnsEmptyStringIfArgumentNil() {
+        XCTAssertEqual(humanize.humanize(nil),"")
+    }
+    
+    func testReturnsEmptyStringForEmptyString(){
+        XCTAssertEqual(humanize.humanize(""),"")
+    }
+    
+    func testReturnsCapitalizedStringIfNoUnderscores(){
+        XCTAssertEqual(humanize.humanize("foo"),"Foo")
+    }
+    
+    func testConvertsUnderscoresToSpaces(){
+        XCTAssertEqual(humanize.humanize("foo_bar_baz"),"Foo bar baz")
+    }
+    
+    func testConvertsUnderscoresToSpacesAgain(){
+        XCTAssertEqual(humanize.humanize("zoo_bar_baz"),"Zoo bar baz")
+    }
+}
+
